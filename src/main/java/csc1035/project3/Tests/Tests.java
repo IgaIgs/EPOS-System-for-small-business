f816ad32a78b598c935fa0074357bdf5019b2d17
B9021107 <c.m.d.barfoot1@newcastle.ac.uk>
2020-02-28 14:14:03 +0000

    Created a README file which has information about what tests are planned to show full functionality of the database which meets the requirements.
    Created Tests.Java class which will run our tests to show all the requirements have been implemented and are working as intended

--- src/main/java/csc1035/project3/Tests/Tests.java
+++ src/main/java/csc1035/project3/Tests/Tests.java
@@ -1,4 +1,31 @@
 package csc1035.project3.Tests;
 
 public class Tests {
+    public static void main(String[] args) {
+
+        //Create new product
+
+
+
+        //Read the database and find our new product
+
+
+
+        //Update some of the attributes of our new product
+
+
+
+        //Delete our new product
+
+
+
+        //Generate a receipt from a simulated transaction
+
+
+
+        //Update stock levels of a few products
+
+
+
+    }
 }
