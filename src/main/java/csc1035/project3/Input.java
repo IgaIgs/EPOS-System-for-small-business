2a80c815272017199505deafd8c394c59ac0750c
b9021380 <b9021380@newcastle.ac.uk>
2020-02-28 15:35:58 +0000

    Began input class. Doesnt Work yet

---
+++ src/main/java/csc1035/project3/Input.java
@@ -0,0 +1,38 @@
+package csc1035.project3;
+import java.util.Scanner;
+public class Input{
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("What would you like to do today?" + "\n" +
+                "1. Sell item"  + "\n" +
+                "2. Edit stock"  + "\n" +
+                "3. Check stock" + "\n" +
+                "4. Exit");
+
+        String theCommand = userInput.nextLine();
+
+        if (theCommand == "1") {
+
+
+        }
+
+        if (theCommand == "2"){
+            System.out.println("1.Add Stock" + "\n" +
+                    "2. Remove Stock" + "\n" +
+                    "3. Edit product attributes");
+
+            if (theCommand == "1"){
+                //Run Code from other team members
+            }
+
+
+        }
+        if (theCommand == "3") {
+
+        }
+
+        if (theCommand == "4") {
+            System.exit(0);
+        }
+    }
+}
