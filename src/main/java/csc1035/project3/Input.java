56547f2f4e57caa3ccaf38892ec9211c7ecb783d
b9021380 <b9021380@newcastle.ac.uk>
2020-03-03 13:17:32 +0000

    Used '.equals()' instead of '==' which solved an error. Still not optimized

--- src/main/java/csc1035/project3/Input.java
+++ src/main/java/csc1035/project3/Input.java
@@ -10,37 +10,36 @@ public class Input{
                 "4. Exit");
 
         String theCommand = userInput.nextLine();
-
-        if (theCommand == "1") {
+        if (theCommand.equals("1")) {
             System.out.println("What would you like to sell?" + "\n" +
                      "1. Enter product name:" + "\n" +
                     "2. Back");
-            String newCommand = userInput.nextLine();
 
-            if (newCommand == "1"){
+            String newCommand = userInput.nextLine();
+            if (newCommand.equals("1")){
                 //Run Code from other team members
+            }
+
+            if (newCommand.equals("2")){
+
             }
 
 
         }
 
-        if (theCommand == "2"){
+        if (theCommand.equals("2")){
             System.out.println("1.Add Stock" + "\n" +
                     "2. Remove Stock" + "\n" +
                     "3. Edit product attributes");
             String newCommand = userInput.nextLine();
-
-            if (newCommand == "1"){
+            if (newCommand.equals("1")){
                 //Run Code from other team members
             }
-
+        }
+        if (theCommand.equals("3")) {
 
         }
-        if (theCommand == "3") {
-
-        }
-
-        if (theCommand == "4") {
+        if (theCommand.equals("4")) {
             System.exit(0);
         }
     }
