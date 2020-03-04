2645ceafc8efc14ad336008be148c4df39fa0b18
b9021380 <b9021380@newcastle.ac.uk>
2020-03-04 11:11:55 +0000

    Basic Menu system finally created because I am incompetent. Used loops like I should have from the beginning. Further changes to menu pending depending on further changes from team.

--- src/main/java/csc1035/project3/Input.java
+++ src/main/java/csc1035/project3/Input.java
@@ -1,46 +1,57 @@
 package csc1035.project3;
 import java.util.Scanner;
-public class Input{
+public class Input {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
-        System.out.println("What would you like to do today?" + "\n" +
-                "1. Sell item"  + "\n" +
-                "2. Edit stock"  + "\n" +
-                "3. Check stock" + "\n" +
-                "4. Exit");
+        String menuStatus = "0";
+        while (menuStatus.equals("0")) {
+            System.out.println("What would you like to do today?" + "\n" +
+                    "1. Sell item" + "\n" +
+                    "2. Edit stock" + "\n" +
+                    "3. Check stock" + "\n" +
+                    "4. Exit");
+            String theCommand = userInput.nextLine();
+            while (theCommand.equals("1")) {
+                System.out.println("What would you like to sell?" + "\n" +
+                        "1. Enter product number:" + "\n" +
+                        "2. Display product categories" + "\n" +
+                        "3. Back");
 
-        String theCommand = userInput.nextLine();
-        if (theCommand.equals("1")) {
-            System.out.println("What would you like to sell?" + "\n" +
-                     "1. Enter product name:" + "\n" +
-                    "2. Back");
-
-            String newCommand = userInput.nextLine();
-            if (newCommand.equals("1")){
-                //Run Code from other team members
+                String newCommand = userInput.nextLine();
+                if (newCommand.equals("1")) {
+                    //Blah Blah Blah
+                }
+                if (newCommand.equals("2")) {
+                    //Blah Blah Blah
+                } else if(newCommand.equals("3")){
+                    break;
+                }
             }
-
-            if (newCommand.equals("2")){
+            while (theCommand.equals("2")) {
+                System.out.println("1. Add Stock" + "\n" +
+                        "2. Remove Stock" + "\n" +
+                        "3. Edit product attributes" + "\n" +
+                        "4. Back");
+                String newCommand = userInput.nextLine();
+                if(newCommand.equals("1")){
+                    //Blah Blah Blah
+                }
+                if (newCommand.equals("2")){
+                    //Blah Blah Blah
+                }
+                if(newCommand.equals("3")){
+                    //Blah Blah Blah
+                }else if(newCommand.equals("4")){
+                    break;
+                }
 
             }
+            while(theCommand.equals("3")) {
 
-
-        }
-
-        if (theCommand.equals("2")){
-            System.out.println("1.Add Stock" + "\n" +
-                    "2. Remove Stock" + "\n" +
-                    "3. Edit product attributes");
-            String newCommand = userInput.nextLine();
-            if (newCommand.equals("1")){
-                //Run Code from other team members
             }
-        }
-        if (theCommand.equals("3")) {
-
-        }
-        if (theCommand.equals("4")) {
-            System.exit(0);
+            while (theCommand.equals("4")) {
+                System.exit(0);
+            }
         }
     }
 }
