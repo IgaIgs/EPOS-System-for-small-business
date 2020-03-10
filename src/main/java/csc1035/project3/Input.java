2c92c11216286ae82692285edc48ecef7963f371
b9021380 <b9021380@newcastle.ac.uk>
2020-03-10 14:50:20 +0000

    Began implementing methods from CRUDTeam. Not finished yet :(

--- src/main/java/csc1035/project3/Input.java
+++ src/main/java/csc1035/project3/Input.java
@@ -18,8 +18,13 @@ public class Input {
             int theCommand = userInput.nextInt();
 
             while (theCommand == 1) {
-                System.out.println("Select the item you would like to sell");
-                System.out.println(getID());
+                System.out.println("What would you like to sell?");
+                int id = getID();
+                System.out.println("How much would you like to sell?");
+                int qty = userInput.nextInt();
+                cI.addToBasket(id, qty);
+                System.out.println("You have added " + qty + " " + id + "to the basket");
+                break;
             }
 
             while (theCommand == 2) {
@@ -27,34 +32,42 @@ public class Input {
                         "2. Remove Stock" + "\n" +
                         "3. Edit product attributes" + "\n" +
                         "4. Back");
+
                 String newCommand = userInput.nextLine();
 
                 if(newCommand.equals("1")){
-                    //Prompt user to input the appropriate details for each product attribute.
-                    //Eg; name, price and so on
-                    //Code for inserting product into database
-                }
-                if (newCommand.equals("2")){
 
-                    //Prompt user to input name or product number
-                    //Display the product and ask if they are sure they want to remove it
-                    //Go back to main menu
                 }
+
+                if (newCommand.equals("2")){
+                    System.out.println("What would you like to remove?");
+                    int id  = getID();
+                    cI.delete(id);
+                }
+
                 if(newCommand.equals("3")){
-                    // insert code for selecting product
-                    // Ask user which attributes need to be changed
+                    int id = getID();
+                    System.out.println("What attribute would you like to update? 'Name' or 'Category'?");
+                    String field = userInput.nextLine();
+                    System.out.println("Input the new value");
+                    String attribute = userInput.nextLine();
+                    cI.updateProduct(id, field, attribute);
+
                 }else if(newCommand.equals("4")){
                     break;
                 }
+            }
 
-            }
             while(theCommand == 3) {
-               // System.out.println(cI.getStock(getID()));
+                int id = getID();
+                // cI.getStock(id);
             }
+
             while(theCommand == 4){
                 System.out.println("Are you sure you want to checkout? Y/N");
                 String YorN = userInput.nextLine();
                 if(YorN.equals("y") || YorN.equals("Y")){
+
                     //Code for printing receipt and stuff goes here
 
                 }else if(YorN.equals("N") || YorN.equals("n")){
@@ -67,6 +80,8 @@ public class Input {
             }
         }
     }
+
+
     private static int getID(){
         // runs the code showing categories etc or the search feature to get ID
         // then return the ID
@@ -80,9 +95,9 @@ public class Input {
             int secondChoice = userInput.nextInt();
             cI.readProduct(secondChoice);
             return userInput.nextInt();
-
         }
         // TODO this is only temporary so that we can run it before we're finished
         return 0;
     }
+
 }
