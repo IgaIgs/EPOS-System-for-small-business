c5449e68c409e3c7d54422a10ede2dbee45b2e4e
B9021107 <c.m.d.barfoot1@newcastle.ac.uk>
2020-03-12 13:54:55 +0000

    Updated tests.java, as this once again had all changes over the past week reverted. Should now be up to date again.

--- src/main/java/Tests/Tests.java
+++ src/main/java/Tests/Tests.java
@@ -11,68 +11,59 @@ public class Tests {
         Session session = HibernateUtil.getSessionFactory().openSession();
         CRUDInterface cI = new CRUDTeamDb();
 
-        /*
-        //Create new product - Works fine
-        Product createProduct = new Product("TestOneCreate", "NewCat", false, 10.0, 10, 10.0);
-        cI.create(createProduct);
+        /**
+         //Create new product
+         Product createProduct = new Product("TestOneCreate", "NewCat", false, 10.0, 10, 10.0);
+         cI.create(createProduct);
 
 
-        //Update the values of a test product
-        cI.updateProduct(10, "cost", "420");
-        cI.updateProduct(10, "perishable", "false");
-        //cI.updateProduct(10, "prodCat", "newCat");
-        //cI.updateProduct(10, "prodName", "newName");
-        cI.updateProduct(10, "sell_price", "777");
-        cI.updateProduct(10, "stock", "9001");
+         //Update the values of a test product - The first parameter needs to be updated to the respective product ID
+         cI.updateProduct(10, "cost", "420");
+         cI.updateProduct(10, "perishable", "false");
+         cI.updateProduct(10, "prodCat", "newCat");
+         cI.updateProduct(10, "prodName", "newName");
+         cI.updateProduct(10, "sell_price", "777");
+         cI.updateProduct(10, "stock", "9001");
 
 
-        //Return the ID's of products whose name contains our parameter - Works fine
-        cI.readByProdNameReturnId("a");
+         //Return the ID's of products whose name contains our parameter (this can be changed as required)
+         cI.readByProdNameReturnId("a");
 
 
-        //Update stock levels of a few products - Works fine
-        cI.updateProduct(32, "stock", "141909");
+         //Update stock levels of a few products - First parameter is our product ID, third parameter our new value
+         cI.updateProduct(10, "stock", "141");
 
 
-        //Delete the product whose values we just updated - Works fine
-        cI.delete(32);
-        */
+         //Delete a product - Only parameter is product ID
+         cI.delete(32);
 
 
-        //Selling an item works, and produces a receipt. - Works fine
-
-        //cI.sellItem(4, 1, 100);
-
-        //Selling wrong amounts of items (negative)
-
-        //TODO To be implemented - Awaiting UI to start this test
+         //Sell an item and produce a receipt.
+         cI.sellItem(4, 1, 100);
 
 
-        //IDs that don't exist - Works fine
+         //IDs that don't exist - No error messages will be thrown and no updates will occur in DB
+         cI.updateProduct(10000, "stock", "420");
 
 
-        //cI.updateProduct(10000, "stock", "420");
+         //Not enough money
+         cI.sellItem(4, 1, 70);
 
 
-        //Not enough money
-
-        //TODO To be implemented -  NEEDS TO BE FIXED, transaction still goes through
-
-        //cI.sellItem(4, 1, 70);
-
-        //Being able to sell multiple items at once
-
-
-        //TODO To be implemented
+         //Add items to our basket and buy all of them in one purchase. Parameters for add to basket are product ID
+         //and amount, parameter for checkout is money given.
+         cI.addToBasket(11, 3);
+         cI.addToBasket(10, 1);
+         cI.addToBasket(8, 2);
+         cI.checkout(1000);
 
 
-        //Not enough stock to sell
+         //Not enough stock to sell
+         cI.sellItem(4, 10000, 1000000);
 
-        //TODO To be implemented - NEEDS TO BE FIXED, To be fixed by changing line 233 in CRUDTeamDB
 
-        //cI.sellItem(4, 10000, 1000000);
-
-        //We will close our session after all our tests have ran.
-        session.close();
+         //We will close our session after all our tests have ran.
+         session.close();
+         **/
     }
 }
